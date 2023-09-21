0bd707cbe19e859cf10e72e45f95c878c8aa96fd
Matyáš Strelec <matyasstrelec@gmail.com>
2023-09-21 14:15:59 +0200

    Initial commit

---
+++ departures/departuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  departuresApp.swift
+//  departures
+//
+//  Created by Matyáš Strelec on 21/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct departuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
